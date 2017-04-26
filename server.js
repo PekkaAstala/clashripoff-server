38b1088d24af85ed8f1e4a82e407845e9d7bd077
Sami Kurvinen <sami.kurvinen@gmail.com>
2017-04-26 21:19:49 +0300

    Install nodemon

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = express()
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+  console.log('Client connected!');
   ws.on('close', () => console.log('Client disconnected'));
 });
 
